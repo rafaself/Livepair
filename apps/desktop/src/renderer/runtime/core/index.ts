fd3a8c8cced42440db00f8cbe32bbb837927644c
rafa <rafaondjango@gmail.com>
2026-03-11 23:43:28 -0300

    refactor(desktop): drop unused runtime barrels

--- apps/desktop/src/renderer/runtime/core/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './session.types';
-export * from './sessionControllerTypes';
-export * from './defaults';
-export * from './runtimeUtils';
-export * from './logger';
--- apps/desktop/src/renderer/runtime/transport/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './geminiLiveSdkClient';
-export * from './geminiLiveTransport';
-export * from './transportEventRouter';
-export * from './liveConfig';
